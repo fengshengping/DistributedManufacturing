75c6ca6bbec91fca7dee262029e1719f9a8c49e5
fengshengping <65514663+fengshengping@users.noreply.github.com>
2020-05-18 11:06:32 -0500

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const history = require("connect-history-api-fallback");
+const jsonServer = require("json-server");
+const bodyParser = require('body-parser');
+const auth = require("./authMiddleware");
+const router = jsonServer.router("serverdata.json");
+const enableHttps = true;
+const ssloptions = {}
+if (enableHttps) {
+ ssloptions.cert = fs.readFileSync("./ssl/DistributedManufacturing.crt");
+ ssloptions.key = fs.readFileSync("./ssl/DistributedManufacturing.pem");
+}
+const app = express();
+app.use(bodyParser.json());
+app.use(auth);
+app.use("/api", router);
+app.use(history());
+app.use("/", express.static("./dist/DistributedManufacturing"));
+app.listen(80,
+ () => console.log("HTTP Server running on port 80"));
+if (enableHttps) {
+ https.createServer(ssloptions, app).listen(443,
+ () => console.log("HTTPS Server running on port 443"));
+} else {
+ console.log("HTTPS disabled")
+}
